d757e4d0c58ca92836357d2c7ab24c24757f30e5
WoochanKim <woochankim200@gmail.com>
2020-06-01 00:55:56 +0900

    Constant study 01

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,8 +1,13 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
     // if there is no mut keyword, it will occur compile error.
     // because basically variable is constant.
     let mut x = 5;
     println!("The value of x is {}.", x);
-    x = 6;
+
+    x += 2;
     println!("The value of x is {}.", x);
+
+    println!("The value of MAX_POINTS is {}.", MAX_POINTS);
 }
